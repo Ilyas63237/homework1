d1d88b3a90f404675e34826347b86564f6e876c6
Ilyas63237 <vakhitov.ilyas@bk.ru>
2024-07-08 10:43:03 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,5 @@
+works = 12
+hours = 1.5
+well = 'Python'
+time_work = hours/works
+print('Курс: ' + str(well) + ', всего задач: ' + str(works) + ', затрачено часов: ' + str(hours) + ' среднее время  выполнения: ' + str(time_work))
